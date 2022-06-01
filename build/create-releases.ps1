a1d6096f8eb5243110229bef2859d9a6f62003fd
Lisandro Lorea <lisandrolorea@gmail.com>
2022-06-01 17:21:17 -0300

    Auto open browser to draft new release

--- build/create-releases.ps1
+++ build/create-releases.ps1
@@ -4,3 +4,4 @@ $fullName = $baseName + $vtag
 Rename-Item -Path '.\Godot Version Manager-osx.zip' -NewName "$fullName-osx.zip"
 Compress-Archive -Path .\win\gvm.exe -DestinationPath "$fullName-win.zip"
 Compress-Archive -Path .\linux\gvm.x86_64 -DestinationPath "$fullName-x11.zip"
+Start-Process "https://github.com/noidexe/godot-version-manager/releases/new"
