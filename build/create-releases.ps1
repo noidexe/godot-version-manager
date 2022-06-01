83dddbee394d76d2d7e07956a50ea9ce1a211f82
Lisandro Lorea <lisandrolorea@gmail.com>
2022-06-01 17:15:33 -0300

    Add basic script for creating releases

---
+++ build/create-releases.ps1
@@ -0,0 +1,6 @@
+﻿$baseName = "Godot Version Manager "
+$vtag = Read-Host "Type verion tag: "
+$fullName = $baseName + $vtag
+Rename-Item -Path '.\Godot Version Manager-osx.zip' -NewName "$fullName-osx.zip"
+Compress-Archive -Path .\win\gvm.exe -DestinationPath "$fullName-win.zip"
+Compress-Archive -Path .\linux\gvm.x86_64 -DestinationPath "$fullName-x11.zip"
