b05c79f6a5d5c4b66059e300ee6ace1eec955e40
Lisandro Lorea <lisandrolorea@gmail.com>
2023-01-19 04:13:01 -0300

    Switch NewsFeed.gd to use HTMLObject

--- scenes/NewsFeed.gd
+++ scenes/NewsFeed.gd
@@ -26,6 +26,7 @@ func _refresh_news():
 	
 	loading_text.hide()
 	var news = _get_news(response[3])
+	
 	_save_news_cache(news)
 	_update_news_feed(news)
 
@@ -71,108 +72,49 @@ func _save_news_cache(news : Array):
 # Analyzes html for <div class="news-item"> elements
 # which will be further parsed by _parse_news_item()
 func _get_news(buffer) -> Array:
-
-	var xml = XMLParser.new()
-	var error = xml.open_buffer(buffer)
-
-	if error == OK:
-		while(true):
-			var err = xml.read()
-
-			if err != OK:
-				if err != ERR_FILE_EOF:
-					print("Error %s reading XML" % err)
-				break
-
-			# Look for <div class="posts"> element
-			if xml.get_node_type() == XMLParser.NODE_ELEMENT and xml.get_node_name() == "div":
-				var class_attr = xml.get_named_attribute_value_safe("class")
-				# Take note of the offsets from <div class="posts"> to </div>
-				# to further analyze
-				if "posts" in class_attr:
-					var tag_open_offset = xml.get_node_offset()
-					_skip_section_handle_void_elements(xml)
-					var tag_close_offset = xml.get_node_offset()
-					return _parse_posts(buffer, tag_open_offset, tag_close_offset)
-	else:
-		print("Error %s getting download info" % error)
-	# return an empty array by default
-	return []
-
-
-
-func _parse_posts(buffer, begin_ofs, end_ofs):
 	var parsed_news = []
 	
-	var xml = XMLParser.new()
-	var error = xml.open_buffer(buffer)
-	if error != OK:
-		printerr("Error parsing news item. Error code: %s" % error)
-	xml.seek(begin_ofs) # automatically does xml.read()
-	while(xml.get_node_offset() != end_ofs):
-		if xml.get_node_type() == XMLParser.NODE_ELEMENT:
-			match xml.get_node_name():
-				"a":
-					var tag_open_offset = xml.get_node_offset()
-					_skip_section_handle_void_elements(xml)
-					var tag_close_offset = xml.get_node_offset()
-					parsed_news.append(_parse_news_item(buffer, tag_open_offset, tag_close_offset))
-		xml.read()
+	var html = HTMLObject.new()
+	html.load_from_buffer(buffer)
+	
+	var posts : HTMLObject.HTMLNodeList = html.of_class("posts").first().children.with_name("a")
+	for post in posts:
+		post = post as HTMLObject.HTMLNode
+		var parsed_item = {}
+		# LINK
+		parsed_item["link"] = BASE_URL + post.attributes.get("href")
+		
+		# IMAGE
+		var divs : HTMLObject.HTMLNodeList = post.children.with_name("article").first().children.with_name("div")
+		var image_style = divs.of_class("thumbnail").first().attributes.get("style")
+		var url_start = image_style.find("'") + 1
+		var url_end = image_style.find_last("'")
+		var image_url = image_style.substr(url_start,url_end - url_start)
+		parsed_item["image"] = BASE_URL + image_url
+		
+		var content : HTMLObject.HTMLNode = divs.of_class("content").first()
+		# CONTENTS
+		parsed_item["contents"] = content.children.with_name("p").first().children.first().value.strip_edges()
+		
+		# TITLE
+		parsed_item["title"] = content.children.with_name("h3").first().children.first().value.strip_edges()
+		var info : HTMLObject.HTMLNodeList = content.children.of_class("info").first().children
+		
+		# AVATAR, AUTHOR, DATE
+		for node in info:
+			node = node as HTMLObject.HTMLNode
+			match node.attributes.get("class"):
+				"avatar":
+					parsed_item["avatar"] = BASE_URL + node.attributes.get("src")
+				"by":
+					parsed_item["author"] = node.children.first().value.strip_edges()
+				"date":
+					parsed_item["date"] = node.children.first().value.strip_edges().lstrip("&nbsp;-&nbsp;")
+
+		parsed_news.append(parsed_item)
 	return parsed_news
 
 
-# Extract the necesary info for each news item
-func _parse_news_item(buffer, begin_ofs, end_ofs):
-	var parsed_item = {}
-	var xml = XMLParser.new()
-	var error = xml.open_buffer(buffer)
-	if error != OK:
-		printerr("Error parsing news item. Error code: %s" % error)
-	xml.seek(begin_ofs) # automatically does xml.read()
-	
-	# We iterate over every node in the range specified by
-	# begin_ofs and end_ofs fetching the info we care about
-	# strip_edges is needed since text nodes seem to contain
-	# every character as it is in the html, including
-	# tabulation and leading spaces
-	while(xml.get_node_offset() != end_ofs):
-		if xml.get_node_type() == XMLParser.NODE_ELEMENT:
-			match xml.get_node_name():
-				"a":
-					parsed_item["link"] = BASE_URL + xml.get_named_attribute_value_safe("href")
-				"img":
-					if "avatar" in xml.get_named_attribute_value_safe("class"):
-						parsed_item["avatar"] = BASE_URL + xml.get_named_attribute_value_safe("src")
-				"div":
-					if "thumbnail" in xml.get_named_attribute_value_safe("class"):
-						var image_style = xml.get_named_attribute_value_safe("style")
-						var url_start = image_style.find("'") + 1
-						var url_end = image_style.find_last("'")
-						var image_url = image_style.substr(url_start,url_end - url_start)
-						parsed_item["image"] = BASE_URL + image_url
-				"h3":
-					xml.read()
-					parsed_item["title"] = xml.get_node_data().strip_edges() if xml.get_node_type() == XMLParser.NODE_TEXT else ""
-				"h4":
-					if "author" in xml.get_named_attribute_value_safe("class"):
-						xml.read()
-						parsed_item["author"] = xml.get_node_data().strip_edges() if xml.get_node_type() == XMLParser.NODE_TEXT else ""
-				"span":
-					if "date" in xml.get_named_attribute_value_safe("class"):
-						xml.read()
-						parsed_item["date"] = xml.get_node_data().strip_edges().lstrip("&nbsp;-&nbsp;") if xml.get_node_type() == XMLParser.NODE_TEXT else ""
-					if "by" in xml.get_named_attribute_value_safe("class"):
-						xml.read()
-						parsed_item["author"] = xml.get_node_data().strip_edges() if xml.get_node_type() == XMLParser.NODE_TEXT else ""
-				"p":
-					if "excerpt" in xml.get_named_attribute_value_safe("class"):
-						xml.read()
-						parsed_item["contents"] = xml.get_node_data().strip_edges() if xml.get_node_type() == XMLParser.NODE_TEXT else ""
-		xml.read()
-		
-	# Return the dictionary with the news entry once we are done
-	return parsed_item
-	
 func _on_screen_resized():
 	visible = get_viewport_rect().size.x > 1046
 		
