77b517c35bcbe1f2c5585c68b0d3155aa38f3d4f
Lisandro Lorea <lisandrolorea@gmail.com>
2023-02-03 20:39:04 -0300

    Fix Open Download Folder not working on OSX

--- scenes/OpenFolder.gd
+++ scenes/OpenFolder.gd
@@ -1,5 +1,13 @@
 extends Button
 
 func _on_OpenFolder_pressed():
-# warning-ignore:return_value_discarded
-	OS.shell_open(ProjectSettings.globalize_path("user://versions/"))
+	var dir = Directory.new()
+	if not dir.dir_exists("user://versions/"):
+		var err = dir.make_dir("user://versions/")
+		print_debug(err)
+	var path = ProjectSettings.globalize_path("user://versions/")
+	if OS.has_feature("OSX"):
+		path = "file://" + path
+	var err = OS.shell_open(path)
+	if err != OK:
+		print_debug(err)
