240459a27508442215966f966a61be89d722bdd2
Lisandro Lorea <lisandrolorea@gmail.com>
2024-03-27 11:37:04 -0300

    Some changes to dark mode
    - Switch to dropdown
    - Remove timer. If proved to be an actual problem, debouncing should be
    added in Config.write_config(). The timer also introduced problems like
    closing the app before the setting was saved
    - Move installed and panel styleboxes to .tres files for easy editing

--- scenes/UIDarkMode.gd
+++ scenes/UIDarkMode.gd
@@ -1,15 +1,29 @@
 extends HBoxContainer
 
 
+enum Mode { LIGHT, DARK }
+
+
 # Path to the Main Node to edit its theme
-onready var main_node = $"../../../../.."
+export var main_node_path : NodePath
+onready var main_node : Control
 
 # Custom Styles from some nodes, not darkmode friendly but a work around
 onready var install_style: StyleBoxFlat = $"%Installed".get("custom_styles/bg")
-onready var logo_panel_style: StyleBoxFlat = $"../../LogoContainer/Panel".get("custom_styles/panel")
+onready var logo_panel_style: StyleBoxFlat = $"%LogoContainer/Panel".get("custom_styles/panel")
+
+var installed_style_dark : StyleBoxFlat = preload("res://theme/installed_dark.tres")
+var logo_panel_style_dark : StyleBoxFlat = preload("res://theme/logo_panel_dark.tres")
+
 
 
 func _ready():
+	main_node = get_node_or_null(main_node_path)
+	assert(is_instance_valid(main_node))
+	$OptionButton.clear()
+	$OptionButton.add_item("Light", Mode.LIGHT)
+	$OptionButton.add_item("Dark", Mode.DARK)
+	
 	var config = Globals.read_config()
 	# adds the darkmode setting if not existing
 	if not "darkmode" in config["ui"]:
@@ -17,10 +31,11 @@ func _ready():
 		Globals.write_config(config)
 	
 	# sets the button to its state and enable it so it doesn't click it on itself
-	$CheckButton.pressed = config.ui.get("darkmode", false)
-	$CheckButton.disabled = false
-
-	mode_handler($CheckButton.pressed)
+	var darkmode_enabled = config.ui.get("darkmode", false)
+	$OptionButton.select(Mode.DARK if darkmode_enabled else Mode.LIGHT)
+	$OptionButton.disabled = false
+	
+	mode_handler(darkmode_enabled)
 
 
 # Toggles between dark and main theme
@@ -29,57 +44,24 @@ func mode_handler(dark: bool):
 		true:
 			main_node.set_theme(load("res://theme/dark_mode.theme"))
 			# Tweaks the custom override styles that has been set to dark
-			$"%Installed".set("custom_styles/bg", get_dark_installed_style())
-			$"../../LogoContainer/Panel".set("custom_styles/panel", get_dark_logo_panel_style())
+			$"%Installed".set("custom_styles/bg", installed_style_dark)
+			$"%LogoContainer/Panel".set("custom_styles/panel", logo_panel_style_dark)
 		false:
 			main_node.set_theme(load("res://theme/main.theme"))
 			# Tweaks the custom override styles to normal
 			$"%Installed".set("custom_styles/bg", install_style)
-			$"../../LogoContainer/Panel".set("custom_styles/panel", logo_panel_style)
+			$"%LogoContainer/Panel".set("custom_styles/panel", logo_panel_style)
 
 
-# new style for installedlist
-func get_dark_installed_style() -> StyleBoxFlat:
-	var new_installed_style = StyleBoxFlat.new()
-	new_installed_style.set_bg_color(Color(0.09, 0.09, 0.09, 1.0))
-	new_installed_style.border_width_bottom = 1
-	new_installed_style.border_width_top = 1
-	new_installed_style.border_width_left = 1
-	new_installed_style.border_width_right = 1
-	new_installed_style.border_color = Color("#ffffff")
-	new_installed_style.corner_radius_bottom_left = 10
-	new_installed_style.corner_radius_bottom_right = 10
-	new_installed_style.corner_radius_top_left = 10
-	new_installed_style.corner_radius_top_right = 10
-	new_installed_style.content_margin_left = 20
-	new_installed_style.content_margin_right = 20
-	new_installed_style.content_margin_top = 30
-	new_installed_style.content_margin_bottom = 20
-	
-	return new_installed_style
-
-
-func get_dark_logo_panel_style() -> StyleBoxFlat:
-	var new_logo_panel_style = StyleBoxFlat.new()
-	new_logo_panel_style.set_bg_color(Color(0.09, 0.09, 0.09, 1.0))
-	new_logo_panel_style.border_color = Color("#ffffff")
-	new_logo_panel_style.corner_radius_bottom_left = 2
-	new_logo_panel_style.corner_radius_bottom_right = 2
-	new_logo_panel_style.corner_radius_top_left = 2
-	new_logo_panel_style.corner_radius_top_right = 2
-	return new_logo_panel_style
-
-
-func _on_CheckButton_toggled(button_pressed):
-	mode_handler(button_pressed)
-	$Timer.start(10)
-
-
-# Is a little cooldown so if someone spams the toggler
-# the file doesn't write everytime
-func _on_Timer_timeout():
+func _update_config(darkmode_selected):
 	var config = Globals.read_config()
-	if config["ui"]["darkmode"] == $CheckButton.pressed:
+	if config["ui"]["darkmode"] == darkmode_selected:
 		return
-	config["ui"]["darkmode"] = $CheckButton.pressed
+	config["ui"]["darkmode"] = darkmode_selected
 	Globals.write_config(config)
+
+
+func _on_OptionButton_item_selected(index):
+	var darkmode_selected = $OptionButton.get_item_id(index) == Mode.DARK
+	mode_handler(darkmode_selected)
+	_update_config(darkmode_selected)
